25a97281936fd02e20bf653954fcb79f1cc4ff24
Lucas Siqueira <lucas.siqueira@loovi.com.br>
2022-04-04 11:17:38 -0300

    caixa eletornico implementado

--- exercicios/caixa_eletronico.py
+++ exercicios/caixa_eletronico.py
@@ -1,17 +1,51 @@
-def valor_saque(saque):
-    saque = input("Qual valor vocë deseja sacar? ")
-    
-def calcula_cedulas():
-    nota100 = 100
-    qtd_nota_100 = 0
-    valor = valor_saque(saque)
-    if valor <= nota100:
-        valor_saque - nota100
-        qtd_nota_100 = qtd_nota_100 +1
-        print(f"Fora utilizados {qtd_nota_100} notas de 100 ")
+def calcula_cedula():
+    valor_saque = int(input("Qual valor você deseja sacar? "))
+    cedulas = [100,50,10,5,1]
+    valor_total = valor_saque
+    quantidade_nota_100 = 0
+    quantidade_nota_50 = 0
+    quantidade_nota_10 = 0
+    quantidade_nota_5 = 0
+    quantidade_nota_1 = 0
 
-valor_saque()
-calcula_cedulas()
+    for valor in cedulas:
+        if valor_total >= valor:
+            valor_total = valor_total - valor
+        if valor == 100:
+            quantidade_nota_100 = quantidade_nota_100 + 1
+        if valor == 50:
+            quantidade_nota_50 = quantidade_nota_50 + 1
+        if valor == 10:
+            quantidade_nota_10 = quantidade_nota_10 + 1
+        if valor == 5:
+            quantidade_nota_5 = quantidade_nota_5 + 1
+        if valor == 1:
+            quantidade_nota_1 = quantidade_nota_1 + 1
+        while valor <= valor_total:
+            valor_total = valor_total - valor
+            if valor == 100:
+                quantidade_nota_100 = quantidade_nota_100 + 1
+            if valor == 50:
+                quantidade_nota_50 = quantidade_nota_50 + 1
+            if valor == 10:
+                quantidade_nota_10 = quantidade_nota_10 + 1
+            if valor == 5:
+                quantidade_nota_5 = quantidade_nota_5 + 1
+            if valor == 1:
+                quantidade_nota_1 = quantidade_nota_1 + 1
+        else:
+            continue
+            
+    print(f" Foi necessário {quantidade_nota_100} notas de 100, {quantidade_nota_50} nota/notas de 50, {quantidade_nota_10} nota/notas de 10, {quantidade_nota_5} nota/notas de 5 e {quantidade_nota_1} nota de 1")
+
+   
+calcula_cedula()
+
+
+
+
+
+
     
 
     
