715703f7a1eb0b914ce4ebd294a30bfc3942aba6
Lucas Siqueira <lucas.siqueira@loovi.com.br>
2022-04-04 16:57:51 -0300

    implementando arquivo strings

--- exercicios/string.py
+++ exercicios/string.py
@@ -1,8 +1,17 @@
 texto = input("Digite o texto aqui: ")
 
+def obtem_quantidade_espaco_branco():
+    espaco_em_branco = texto.count(" ")
 
-def calcula_vogal():
-    print(texto.count('aeiou'))
+    print(f"{espaco_em_branco} espaco/espacos vazios")
 
-calcula_vogal()
+def obtem_quanditade_vogais(texto):
+    vogais = {'a': 0, 'e':0, 'i':0, 'o':0, 'u':0}
+    for letra, quantidade in vogais.items():
+        if letra in texto:
+            quantidade = quantidade +1
+        print(letra, quantidade)
+
+obtem_quanditade_vogais(texto)
+
     
