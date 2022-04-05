6790d9b43894b3eab244a7c87a3c63d42d677364
Lucas Siqueira <lucas.siqueira@loovi.com.br>
2022-04-05 10:06:55 -0300

    refatorando e passando o input do texto para dentro da funcao

--- exercicios/string_regex.py
+++ exercicios/string_regex.py
@@ -1,10 +1,14 @@
 import re
 
-texto = input("Digite um texto: ")
 
-def obtem_quantidade_vogal(texto):
+def obtem_quantidade_vogal():
+    texto = input("Digite um texto: ")
+
     vogais = re.findall('[aeiou]', texto, re.IGNORECASE)
+
+    print(f"Possui {len(vogais)} vogais")
+
     return len(vogais)
 
-print(f"Possui {obtem_quantidade_vogal(texto)} vogais")
+obtem_quantidade_vogal()
 
