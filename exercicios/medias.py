68dc23b12396c13e02f6a8124f238438b4cf702e
Lucas Siqueira <lucas.siqueira@loovi.com.br>
2022-04-14 16:39:42 -0300

    Implementando logs no projeto

--- exercicios/medias.py
+++ exercicios/medias.py
@@ -1,5 +1,8 @@
 import re
 import sys
+import logging
+
+logging.basicConfig(filename = 'log.txt', format= '%(levelname)s-%(asctime)s-%(message)s', level=logging.DEBUG)
 
 def executa():
     print("Menu: \n"
@@ -10,6 +13,8 @@ def executa():
     escolha_usuario = input("Escolha uma das opções: ")
 
     verifica_entrada = bool(re.match('^[0-3]$', escolha_usuario))
+
+    logging.info(f"Escolha do usuário: {escolha_usuario}")
     
     if not verifica_entrada:
         print("Código inválido\n")
@@ -20,7 +25,10 @@ def executa():
         sys.exit()
 
     primeiro_valor = input("Digite um valor: ")
+    logging.info(f"Primeiro valor do usuário: {primeiro_valor}")
+
     segundo_valor =  input("Digite um valor: ")
+    logging.info(f"Segundo valor do usuário: {segundo_valor}")
 
     validacao_valor1 = bool(re.match('^\d+$', primeiro_valor))
     validacao_valor2 = bool(re.match('^\d+$', segundo_valor))
@@ -51,13 +59,17 @@ def executa():
 
 
 def calcula_media(primeiro_valor, segundo_valor):
+        logging.info('iniciando calculo da média')
         media = (int(primeiro_valor) + int(segundo_valor)) / 2
 
         print(media)
 
+
 def calcula_media_ponderada(primeiro_valor, segundo_valor, peso1, peso2):
+    logging.info('iniciando calculo da média ponderada')
     if int(peso1 + peso2) == 0:
-        print("Não é possível dividir por 0")
+
+        logging.warning("Erro usuário, Náo é possível realizar divisão por zero")
 
     else:
         media_ponderada  = (int(primeiro_valor) * int(peso1) + int(segundo_valor) * int(peso2)) / int(peso1)+ int(peso2)
